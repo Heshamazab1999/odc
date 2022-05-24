5124fa9ce3988fba65912e24f7910794360a74c0
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2022-05-24 15:24:42 -0700

    Initial commit

---
+++ lib/controller/home_controller.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:odc_hackathon/controller/base_controller.dart';
+import 'package:odc_hackathon/enum/view_state.dart';
+import 'package:odc_hackathon/model/CategoryDetails.dart';
+import 'package:odc_hackathon/model/CategoryModel.dart';
+import 'package:odc_hackathon/model/CoursesDetails.dart';
+import 'package:odc_hackathon/model/CoursesModel.dart';
+import 'package:odc_hackathon/services/cache_helper.dart';
+import 'package:odc_hackathon/services/home_services.dart';
+import 'package:get/get.dart';
+import 'package:odc_hackathon/services/login_services.dart';
+import 'package:odc_hackathon/views/login/login.dart';
+
+class HomeController extends BaseController {
+  static HomeController to = Get.find();
+  final _services = HomeServices();
+  final _logout = LoginServices();
+  CategoryModel? categoryModel;
+  CategoryDetailsModel? detailsModel;
+  CoursesModel? coursesModel;
+  CoursesDetails? coursesDetails;
+
+  @override
+  Future<void> onInit() async {
+    // ignore: todo
+    // TODO: implement onInit
+    super.onInit();
+    setSate(ViewState.busy);
+    categoryModel = await _services.getAllCategories();
+    coursesModel = await _services.getAllCourses();
+    setSate(ViewState.idle);
+  }
+
+  getCategoryDetails(int id) async {
+    detailsModel = await _services.getCategoryDetails(id);
+  }
+
+  getCoursesDetails(int id) async {
+    coursesDetails = await _services.getAllCoursesDetails(id);
+  }
+
+  logout(BuildContext context) async {
+    _logout.logout();
+    CacheHelper.removeData(key: "token");
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const Login()),
+    );
+  }
+}
